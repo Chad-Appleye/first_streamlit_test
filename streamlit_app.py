ba1a5b451d8cd6312a6e8a6ccd053b75ef91bd7b
Chad Apuli <123600762+Chad-Appleye@users.noreply.github.com>
2023-02-03 14:57:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,7 @@ if add_sidebar == 'Alignments':
     alignment_file = st.file_uploader(label='Alignment File', help="Upload a FASTA alignment file")
     if alignment_file is not None:
       alignment_df = pd.read_table(alignment_file)
+      st.write(alignment_df)
       
 #       byte_str = alignment_file.read()
 #       text_obj = byte_str.decode('UTF-8')
