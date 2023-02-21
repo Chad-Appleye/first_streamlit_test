0aae2b33b938f08ffa3f17584f780e879aab162c
Chad Apuli <123600762+Chad-Appleye@users.noreply.github.com>
2023-02-21 10:11:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,9 @@ if add_sidebar == 'Alignments':
     alignment_file = st.file_uploader(label='Alignment File', help="Upload a FASTA alignment file")
     if alignment_file is not None:
         alignment = pd.read_table(alignment_file, header=None)
-        st.write(alignment)
+        name_list = alignment.iloc[::2].values.tolist
+        st.write(name_list)
+        
 #         st.write(alignment.iloc[1::2])
         
 #       alignment_df = pd.DataFrame([[alignment.iloc[::2]],[alignment.iloc[1::2]]],columns=['Name', 'Sequence'])
